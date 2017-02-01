59925887ccda8989cff7f44d568c7057aba5f4d5
Tom Burnett <t_burnett@outlook.coml>
2017-02-01 23:45:14 +0000

    Add Todo test

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -1,13 +1,28 @@
 import expect from 'expect';
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import TestUtils from 'react-addons-test-utils';
-// import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import $ from 'jquery';
 
 import Todo from 'Todo';
 
 describe('Todo', () => {
   it('should exist', () => {
     expect(Todo).toExist();
+  });
+
+  it('should call onToggle with id', () => {
+    const todoData = {
+      id: 'abc',
+      text: 'Test',
+      completed: true
+    };
+    const spy = expect.createSpy();
+    const todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy}/>);
+    const $el = $(ReactDOM.findDOMNode(todo));
+
+    TestUtils.Simulate.click($el.find('input')[0]);
+
+    expect(spy).toHaveBeenCalledWith(todoData.id);
   });
 });
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -21,4 +21,18 @@ describe('TodoApp', () => {
     expect(todoApp.state.todos.length).toBe(1);
     expect(todoApp.state.todos[0].text).toBe(todoText);
   });
+
+  it('should toggle completed state', () => {
+    const todoData = {
+      id: 'abc',
+      text: 'Test',
+      completed: false
+    };
+    const todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+    todoApp.setState({ todos: [todoData]});
+
+    expect(todoApp.state.todos[0].completed).toBe(false);
+    todoApp.handleToggle('abc');
+    expect(todoApp.state.todos[0].completed).toBe(true);
+  });
 });
